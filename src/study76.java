228b4b083c8cd91a049290dbedaafcea65d908e5
yongsuKim <yongsu0312.kim@gmail.com>
2023-03-02 22:40:24 +0900

    AlgorithmStudy1

--- src/study76.java
+++
@@ -1,5 +0,0 @@
-public class study76 {
-    public static void main(String[] args) {
-        System.out.println("te111111st");
-    }
-}
--- src/study77.java
+++
@@ -1,5 +0,0 @@
-public class study77 {
-    public static void main(String[] args) {
-        System.out.println("te122211st");
-    }
-}
